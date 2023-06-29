9bcd288996f784ed301703b0ca282ae58233cadd
Jesse Cammarano <jcamma93@gmail.com>
2023-06-29 11:03:52 -0400

    Lab Finished

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,19 @@
 alert('WOW!');
+
+fetch('/formsubmissions')
+.then(res => res.json())
+.then(formsubmissions => {
+    formsubmissions.forEach(formsubmission => {
+        const container = document.createElement('div');
+        const h1 = document.createElement('h1');
+        const h2 = document.createElement('h2');
+
+        h1.textContent = formsubmission.name;
+        h2.textContent = formsubmission.email;
+
+        container.appendChild(h1);
+        container.appendChild(h2);
+
+        document.body.appendChild(container);
+    });
+});
--- server/server.js
+++ server/server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const path = require('path');
-// const fs = require('fs');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 
 let app = express();
 
+const FORMSUBMISSIONS_PATH = path.join(__dirname, './formsubmissions.json')
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/contact-form', (req, res) => {
-    console.log(req.body.email);
-    console.log(req.body.name);
-    res.send('Thank you for submitting your contact form.');
-});
+// app.post('/contact-form', (req, res) => {
+//     console.log(req.body.email);
+//     console.log(req.body.name);
+//     res.send('Thank you for submitting your contact form.');
+// });
 
 // middleware logger
 // app.use((req, res, next) => {
@@ -25,12 +27,48 @@ app.post('/contact-form', (req, res) => {
 
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.use(express.urlencoded({ extended:false }))
+
+app.get('/formsubmissions', (req, res) => {
+    fs.readFile(FORMSUBMISSIONS_PATH, (err, data) => {
+        if(err) {
+            res.json({ error: 'Something went wrong.' });
+            return;
+        }
+        const formsubmissions = JSON.parse(data);
+        res.json(formsubmissions);
+
+       
+        })
+    })
 
 
-app.get('/order/:name', (req, res) => {
-    let name = req.params.name;
-    let email = req.query.email;
-    res.send(`Your name is ${name} and email is ${email}`);
-});
+
+
+// app.get('/order/:name', (req, res) => {
+//     let name = req.params.name;
+//     let email = req.query.email;
+//     res.send(`Your name is ${name} and email is ${email}`);
+// });
+
+
+app.post('/contact-form', (req, res) => {
+    fs.readFile(FORMSUBMISSIONS_PATH, (err, data) => {
+        if(err) {
+            res.json({ error: 'Something went wrong.' });
+            return;
+        }
+        const formsubmissions = JSON.parse(data);
+        formsubmissions.push(req.body);
+
+        fs.writeFile(FORMSUBMISSIONS_PATH, JSON.stringify(formsubmissions), (err) => {
+            if(err) {
+                res.json({ error: 'Something went wrong.' });
+                return;
+            }
+        res.json({ msg: 'Thank you for your submission!'})
+        })
+    })
+})
 
 app.listen(3000);
